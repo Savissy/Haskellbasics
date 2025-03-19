8aba24dfbccd004fae178e2b18b47487a8d80134
Savissy <111025155+Savissy@users.noreply.github.com>
2025-03-19 15:48:27 +0100

    Update mock-CIP143.hs

--- mock-cardano/mock-CIP143.hs
+++ mock-cardano/mock-CIP143.hs
@@ -1,10 +1,7 @@
 -- Saviour Uzoukwu
 -- 17th March 2025
-{-# LANGUAGE OverloadedStrings #-}
 import Data.Map (Map)
 import qualified Data.Map as Map
-import Data.Text (Text)
-import Control.Monad (when)
 
 -- Token Definition
 type PolicyID = String
@@ -15,49 +12,35 @@ type UserID = String
 data Token = Token
   { tokenName :: TokenName
   , tokenPolicyID :: PolicyID
-  , tokenAmount :: Amount
-  } deriving (Show, Eq)
-
--- Token Metadata
-data TokenMetadata = TokenMetadata
-  { tokenDescription :: Text
-  , tokenDecimals :: Int
-  } deriving (Show)
-
--- User Balances
-type UserBalances = Map Token Amount
+  } deriving (Show, Eq, Ord)
 
 -- Blockchain State
 data Blockchain = Blockchain
-  { tokens :: Map (PolicyID, TokenName) Token
-  , tokenMetadata :: Map (PolicyID, TokenName) TokenMetadata
-  , userBalances :: Map UserID UserBalances
+  { tokens :: Map (PolicyID, TokenName) Amount  -- Total supply of each token
+  , userBalances :: Map UserID (Map Token Amount)  -- Balances of each user
   } deriving (Show)
 
 -- Initial State
 initialBlockchain :: Blockchain
 initialBlockchain = Blockchain
   { tokens = Map.empty
-  , tokenMetadata = Map.empty
   , userBalances = Map.empty
   }
 
 -- Mint a new token
-mintToken :: Blockchain -> PolicyID -> TokenName -> Amount -> TokenMetadata -> UserID -> Either String Blockchain
-mintToken blockchain policyId name amount metadata userId =
+mintToken :: Blockchain -> PolicyID -> TokenName -> Amount -> UserID -> Either String Blockchain
+mintToken blockchain policyId name amount userId =
   let tokenKey = (policyId, name)
+      token = Token name policyId
   in if Map.member tokenKey (tokens blockchain)
      then Left "Token already exists"
      else
-       let newToken = Token name policyId amount
-           updatedTokens = Map.insert tokenKey newToken (tokens blockchain)
-           updatedMetadata = Map.insert tokenKey metadata (tokenMetadata blockchain)
+       let updatedTokens = Map.insert tokenKey amount (tokens blockchain)
            userBalance = Map.findWithDefault Map.empty userId (userBalances blockchain)
-           updatedUserBalance = Map.insert newToken amount userBalance
+           updatedUserBalance = Map.insert token amount userBalance
            updatedUserBalances = Map.insert userId updatedUserBalance (userBalances blockchain)
        in Right $ blockchain
             { tokens = updatedTokens
-            , tokenMetadata = updatedMetadata
             , userBalances = updatedUserBalances
             }
 
@@ -65,11 +48,11 @@ mintToken blockchain policyId name amount metadata userId =
 transferTokens :: Blockchain -> UserID -> UserID -> PolicyID -> TokenName -> Amount -> Either String Blockchain
 transferTokens blockchain fromUser toUser policyId name amount =
   let tokenKey = (policyId, name)
+      token = Token name policyId
       fromBalance = Map.findWithDefault Map.empty fromUser (userBalances blockchain)
       toBalance = Map.findWithDefault Map.empty toUser (userBalances blockchain)
-      token = Token name policyId amount
   in case (Map.lookup tokenKey (tokens blockchain), Map.lookup token fromBalance) of
-       (Just _, Just fromAmount) ->
+       (Just totalSupply, Just fromAmount) ->
          if fromAmount >= amount
          then
            let updatedFromBalance = Map.insert token (fromAmount - amount) fromBalance
@@ -84,15 +67,15 @@ transferTokens blockchain fromUser toUser policyId name amount =
 burnTokens :: Blockchain -> UserID -> PolicyID -> TokenName -> Amount -> Either String Blockchain
 burnTokens blockchain userId policyId name amount =
   let tokenKey = (policyId, name)
+      token = Token name policyId
       userBalance = Map.findWithDefault Map.empty userId (userBalances blockchain)
-      token = Token name policyId amount
   in case (Map.lookup tokenKey (tokens blockchain), Map.lookup token userBalance) of
-       (Just existingToken, Just userAmount) ->
+       (Just totalSupply, Just userAmount) ->
          if userAmount >= amount
          then
            let updatedUserBalance = Map.insert token (userAmount - amount) userBalance
                updatedUserBalances = Map.insert userId updatedUserBalance (userBalances blockchain)
-               updatedTokens = Map.adjust (\t -> t { tokenAmount = tokenAmount t - amount }) tokenKey (tokens blockchain)
+               updatedTokens = Map.adjust (\supply -> supply - amount) tokenKey (tokens blockchain)
            in Right $ blockchain
                 { tokens = updatedTokens
                 , userBalances = updatedUserBalances
@@ -101,7 +84,7 @@ burnTokens blockchain userId policyId name amount =
        _ -> Left "Token or user not found"
 
 -- Get user balance
-getUserBalance :: Blockchain -> UserID -> Either String UserBalances
+getUserBalance :: Blockchain -> UserID -> Either String (Map Token Amount)
 getUserBalance blockchain userId =
   case Map.lookup userId (userBalances blockchain) of
     Just balance -> Right balance
@@ -112,7 +95,7 @@ main :: IO ()
 main = do
   let blockchain = initialBlockchain
   -- Mint a new token
-  let Right blockchain1 = mintToken blockchain "policy1" "MyToken" 1000 (TokenMetadata "A mock token" 6) "Alice"
+  let Right blockchain1 = mintToken blockchain "policy1" "MyToken" 1000 "Alice"
   -- Transfer tokens
   let Right blockchain2 = transferTokens blockchain1 "Alice" "Bob" "policy1" "MyToken" 100
   -- Burn tokens
