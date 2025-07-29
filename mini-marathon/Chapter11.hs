fadd0561f8a5d2991ce20b6a9ccd42484189113e
Saviour Uzoukwu <111025155+Savissy@users.noreply.github.com>
2025-07-29 03:43:24 -0700

    Update Chapter11.hs

--- mini-marathon/Chapter11.hs
+++ mini-marathon/Chapter11.hs
@@ -5,3 +5,15 @@ main = do
  print "Whats your name"
  print "Nice having you with us"
 
+--Write a program that asks the user for a number, reads it, and prints that number multiplied by 2
+
+multi :: Int -> Int
+multi x = x * 2
+
+main :: IO ()
+main = do
+ putStrLn "Whats your number?"
+ x <- getLine
+ let y = read x :: Int
+ print $ multi y
+
