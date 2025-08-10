5cf456f31d300e9e095616ecd28a68e9f13e72cb
Saviour Uzoukwu <111025155+Savissy@users.noreply.github.com>
2025-08-10 15:00:25 -0700

    Update chapter15.hs

--- mini-marathon/chapter15.hs
+++ mini-marathon/chapter15.hs
@@ -33,6 +33,45 @@ main = do
        then putStrLn "Time cannot be zero."
        else putStrLn $ "Velocity = " ++ show (distance / time) ++ " m/s"
 
+import Control.Exception
+import Data.Char (toUpper)
+
+-- Define our own error type for unknown colors
+data TrafficLightError = UnknownColor String
+  deriving Show
+
+instance Exception TrafficLightError
+
+-- Car reaction function
+reactToLight :: String -> IO ()
+reactToLight color =
+    case map toUpper color of
+        "GREEN"  -> putStrLn "Car is accelerating..."
+        "YELLOW" -> putStrLn "Car is preparing to stop..."
+        "RED"    -> putStrLn "Car is stopping."
+        _        -> throwIO (UnknownColor color)
+
+-- Main program with error handling
+main :: IO ()
+main = do
+    putStrLn "Enter traffic light color (GREEN, YELLOW, RED):"
+    color <- getLine
+
+    -- Try to react and catch any errors
+    catch (reactToLight color)
+          (\(UnknownColor c) -> putStrLn $ "Error: Unknown color '" ++ c ++ "'. Car will stop for safety.")
+
+-- Safe division function
+safeDiv :: (Eq a, Fractional a) => a -> a -> Maybe a
+safeDiv _ 0 = Nothing          -- If denominator is zero, return Nothing
+safeDiv x y = Just (x / y)     -- Otherwise, return the result wrapped in Just
+
+main :: IO ()
+main = do
+    print (safeDiv 10 2)   -- Just 5.0
+    print (safeDiv 7 0)    -- Nothing
+
+
 
     
 
